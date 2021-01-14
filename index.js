af6f3c8d24f148ea7c6371d0db7636bbc3701509
Travis Peterson <contact@travispeterson.us>
2021-01-14 10:55:47 -0600

    fix: removes form8ion dependency

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: '@form8ion/mocha'
+  overrides: [{
+    files: '**/*-test.js',
+
+    env: {mocha: true},
+
+    plugins: ['mocha'],
+
+    extends: ['plugin:mocha/recommended'],
+
+    rules: {
+      'mocha/no-skipped-tests': 'error',
+      'mocha/no-mocha-arrows': 'off',
+      'mocha/no-setup-in-describe': 'off',
+      'mocha/no-hooks-for-single-case': 'off',
+      'filenames/match-regex': ['error', '^[a-z0-9\\-\\.]+-test$'],
+      'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
+    }
+  }]
 };
